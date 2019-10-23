3bb1ff54a2158301ab249862877fdb548c4be000
Leila <nuta19ab@student.cbs.dk>
2019-10-23 12:59:09 +0200

    Table created

--- Leila/Tours.js
+++ Leila/Tours.js
@@ -1,5 +1,5 @@
 
-// object - Tour
+// Tour class
 class Tours {
     constructor(tourName, tourInfo, startTime, duration, meetingPoint, tourPrice, tourLocation) {
         this.tourname = tourName;
@@ -10,54 +10,8 @@ class Tours {
         this.tourPrice = tourPrice;
         this.tourLocation = tourLocation;
     }
-
-   // createHTML(tourTable) {
-        //return $("'<tr><td>' + $(this.tourname´) + '<td><td>' + $("this.startTime") + '</td><td>' + $("this.duration") + '</td><td>' + $("this.tourPrice") + '</td></tr>'");
-
-        //return “<tr><td>“+ this.tourname + “</td><td>” + this.startTime + “</td><td>” + this.duration + “</td><td>” + this.tourPrice + “</td></tr>“;
-
-
-
-    //}
-}
-
-let tour1 = new Tours("Old Copenhagen", "The old city", "12:00", "2 HRS", "Christiansborg", "7 EUR", "Copenhagen");
-let tour2 = new Tours("Copenhagen Higlights", "The little Mermaid, Amalienborg and Nyhavn", "12:00", "1 HR", "Nyhavn", "5 EUR", "Copenhagen");
-let tour3 = new Tours("Rosenborg Castle and Royal Jewelry", "Museum", "11.00", "2 HRS", "Nørreport St.", "10 EUR", "Copenhagen");
-
-var tours = [tour1, tour2, tour3];
-console.log(tours)
-
-var info1 = "";
-//var breakTag = document.createElement("br")
-
-
-//for (tours=0, tours<=)
-for(i=0; i < tours.length; i++){
-  info1 += "Tourname:" + tours[i].tourname +", Tourinfo: " + tours[i].tourInfo +", Start time: " + tours[i].startTime + ", Tour price: " + tours[i].tourPrice;
-//info1 += tours[i].createHTML(tourTable)
-}
-
-
-content.innerText = info1
-
+    createHTML()   {
+        return "<tr><td>" + this.tourname + "</td><td>" + this.startTime + "</td><td>" + this.duration + "</td><td>"+ this.tourPrice + "</td><td>"+ this.tourname + "</td></td>"
+                }
 
 }
-/*Attach an even, we cant to check if the box i open, if yes we want to shrink it vs.  */
-
-/* var content = document.getElementById("content");
-var button = document.getElementById("show-more");
-
-button.onclick = function () {
-
-    if(content.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else{
-        //expand the box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    }
-}; */
-
---
+++ Leila/ToursTable.js
@@ -0,0 +1,20 @@
+// We create different tours with the class function in Tours.js //
+let tour1 = new Tours("Old Copenhagen", "The old city", "12:00", "2 HRS", "Christiansborg", "7 EUR", "Copenhagen");
+let tour2 = new Tours("Copenhagen Higlights", "The little Mermaid, Amalienborg and Nyhavn", "12:00", "1 HR", "Nyhavn", "5 EUR", "Copenhagen");
+let tour3 = new Tours("Rosenborg Castle and Royal Jewelry", "Museum", "11.00", "2 HRS", "Nørreport St.", "10 EUR", "Copenhagen");
+
+// Create an array //
+var tours = [tour1, tour2, tour3];
+console.log(tours)
+
+// We can know call our array in the for loop which will get pushed in our empty string info1 //
+var info1 = "";
+
+for(i=0; i < tours.length; i++){
+    // info1 += "Tourname:" + tours[i].tourname +", Tourinfo: " + tours[i].tourInfo +", Start time: " + tours[i].startTime + ", Tour price: " + tours[i].tourPrice;
+    info1 += tours[i].createHTML()
+}
+
+var table = document.getElementById('hello');
+var tbody = table.getElementsByTagName('tbody');
+tbody[0].innerHTML = info1
