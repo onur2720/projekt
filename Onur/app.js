4be24eb89e52010640661dcd27a82b474517626c
Onur Karakus <onka19ab@student.cbs.dk>
2019-11-08 09:40:56 +0100

    Vores website

--- Onur/app.js
+++ Onur/app.js
@@ -1,34 +1,51 @@
 // Username and Password for the registration form
-var MC = document.getElementById('MC');
-var V = document.getElementById('V');
-var KNR = document.getElementById('KNR');
-var EX = document.getElementById('EX');
-var btnReg = document.getElementById('rgr_btn');
-var TCR = document.getElementById('TCR');
+var arrInformation = JSON.parse(localStorage.getItem('paymentInformation')) || [];
 
 
 function setItem() {
-    localStorage.setItem('MC', MC.value);
-    localStorage.setItem('V', V.value);
-    alert("Kortet er registreret");
-}
-function storeLogin() {
-    // localStorage.setItem('rgr_btn',btnReg.value);
-    console.log(KNR.value.length);
-    if (KNR.value.length < 8) {
-        alert("Der er en fejl ved kortnummeret");
-    }
-    console.log(EX.value.length);
-    if (EX.value.length < 4) {
-        alert("Der en fejl ved udløbsdatoen");
-    }
-    console.log(TCR.value.length);
-    if (TCR.value.length < 3) {
-        alert("Der er en fejl ved Kontrolnummeret");
-    } else {
-        alert('Betalingen er godkendt ');
+    const storeObj = {
+        cardNumber: document.getElementById('KNR').value,
+        expiredDate: document.getElementById('EX').value,
+        controle: document.getElementById('TCR').value
+    };
+
+    for (var i = 0; i < arrInformation.length; i++) {
+        if (arrInformation[i].cardNumber!=8 && arrInformation[i].expiredDate != 4 && arrInformation[i].controle != 3) {
+           // alert("Kortnummer skal være mere end 8 tegn");
+            console.log("testtest");
+            return false;
+        }
+
+
 
     }
+    arrInformation.push(storeObj);
+    localStorage.setItem('paymentInformation', JSON.stringify(arrInformation));
+    return;
+
+
+        // alert("Kortet er registreret");
+
+}
+function storeLogin() {
+
+
+    // // localStorage.setItem('rgr_btn',btnReg.value);
+    // console.log(KNR.value.length);
+    // if (KNR.value.length < 8) {
+    //     alert("Der er en fejl ved kortnummeret");
+    // }
+    // console.log(EX.value.length);
+    // if (EX.value.length < 4) {
+    //     alert("Der en fejl ved udløbsdatoen");
+    // }
+    // console.log(TCR.value.length);
+    // if (TCR.value.length < 3) {
+    //     alert("Der er en fejl ved Kontrolnummeret");
+    // } else {
+    //     alert('Betalingen er godkendt ');
+    //
+    // }
 }
 
 
