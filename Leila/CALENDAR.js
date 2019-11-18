36112106492b206e9badf87e497717f40a68cbbd
Leila <nuta19ab@student.cbs.dk>
2019-11-18 11:58:58 +0100

    kommenteret - LET

--- Leila/CALENDAR.js
+++ Leila/CALENDAR.js
@@ -1,7 +1,10 @@
+// dt is equal to new Date()
 var dt = new Date();
 
 function renderDate() {
+    // our first day in the month is set to the 1st.
     dt.setDate(1);
+    // we get the date from dt with .getDay()
     var day = dt.getDay();
     console.log(dt.getDate());
     console.log(dt.getMonth() + 1);
@@ -9,6 +12,7 @@ function renderDate() {
     var endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
     var prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
 
+    // today is equal to new Date()
     var today = new Date();
     console.log(today);
 
@@ -28,14 +32,15 @@ function renderDate() {
         "December",
     ];
 
-    document.getElementById("date_str").innerHTML = dt.toDateString();
+    document.getElementById("date_str").innerHTML = dt.toLocaleDateString();
     document.getElementById("month").innerHTML = months[dt.getMonth()];
 
+    // the string cells is empty until the a day passes and the dates becomes the previous date and a new class name gets assigned.
     var cells = "";
     for(x = day; x > 0; x--) {
         cells += "<div class='prev_date'>" + (prevDate - x + 1) + "</div>";
     }
-
+// for-loop assigns id for all the dates from the current date to all the upcoming dates
     for(i = 1; i <= endDate; i++){
         if(i == today.getDate() && dt.getMonth() == today.getMonth()) {
             cells += "<div class='today' id='" + i +"'>" + i +  "</div>";
@@ -43,10 +48,10 @@ function renderDate() {
             cells += "<div class='next_date' id='" + i + "'>" + i + "</div>";
         }
     }
-
+// .getElemtsByClassName gives all elements that is assigned the class 'days' from .innerHTML converts to cells.
     document.getElementsByClassName("days")[0].innerHTML = cells;
 }
-
+// function moveDate(para), allows us to move between the months in the calendar based on the conditions in the if/else-statement
 function moveDate(para) {
     if(para == 'prev'){
         dt.setMonth(dt.getMonth()-1);
